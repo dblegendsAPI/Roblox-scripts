399ee9b4f46e809357da346017072c7999bd0a50
Dbl API <143275391+dblegendsAPI@users.noreply.github.com>
2023-09-07 08:38:10 -0400

    Create Infinite jump.lua

---
+++ Infinite jump.lua
@@ -0,0 +1,1 @@
+local infjmp = true game:GetService("UserInputService").jumpRequest:Connect(function() if infjmp then game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass"Humanoid":ChangeState("Jumping") end end)
